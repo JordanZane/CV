5b5aa4d9c3d2062946f822dbe788d778e6a83b51
Zanetti Jordan <jzanetti1@outlook.fr>
2023-04-09 14:45:01 +0200

    Modification styles

--- index.php
+++ index.php
@@ -11,30 +11,32 @@
     </header>
 
     <main>
-        <div class="container">
-            <div class="row">
-                <div class="col-lg-12">
-                    <h2>à propos de <span class="color-blue underline">moi</span></h2>
+        <section class="about-section">
+            <div class="container">
+                    <div class="row">
+                        <div class="col-lg-12">
+                            <h2>à propos de <span class="color-blue underline">moi</span></h2>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col-lg-8">
+                            <p>Attiré par le domaine du web depuis longtemps, ma passion pour le développement web commence en suivant quelques cours sur OpenClassrooms. 
+                                De là, j'entreprend de changer de voie et décide alors d'entreprendre la formation de "développeur web junior". 
+                                Suite à cette formation j'ai obtenu une alternance en tant qu'intégrateur web dans une agence de communication web lors de mon Bachelor. 
+                                J'ai pu ensuite travailler en tant qu'employé et m'épanouir pleinement dans le poste d'intégrateur web.</p>
+                                <p>D'un naturel curieux, je cherche en permanence à développer mes compétences, je suis actuellement à la recherche d'un poste de développeur, intégrateur web. 
+                                N'hésitez pas à me contacter pour de plus amples informations.</p>
+                        </div>
+                        <div class="col-lg-4">
+                            <p><span class="color-blue">Nom :</span> Zanetti Jordan</p>
+                            <p><span class="color-blue">Âge :</span> 27 ans</p>
+                            <p><span class="color-blue">Lieu :</span> Thionville, France</p>
+                            <p><span class="color-blue">Hobbies :</span> Numérique, informatique, sports, jeux-vidéo</p>
+                            <p>Permis B</p>
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div class="row">
-                <div class="col-lg-8">
-                    <p>Attiré par le domaine du web depuis longtemps, ma passion pour le développement web commence en suivant quelques cours sur OpenClassrooms. 
-                        De là, j'entreprend de changer de voie et décide alors d'entreprendre la formation de "développeur web junior". 
-                        Suite à cette formation j'ai obtenu une alternance en tant qu'intégrateur web dans une agence de communication web lors de mon Bachelor. 
-                        J'ai pu ensuite travailler en tant qu'employé et m'épanouir pleinement dans le poste d'intégrateur web.</p>
-                        <p>D'un naturel curieux, je cherche en permanence à développer mes compétences, je suis actuellement à la recherche d'un poste de développeur, intégrateur web. 
-                        N'hésitez pas à me contacter pour de plus amples informations.</p>
-                </div>
-                <div class="col-lg-4">
-                    <p><span class="color-blue">Nom :</span> Zanetti Jordan</p>
-                    <p><span class="color-blue">Âge :</span> 27 ans</p>
-                    <p><span class="color-blue">Lieu :</span> Thionville, France</p>
-                    <p><span class="color-blue">Hobbies :</span> Numérique, informatique, sports, jeux-vidéo</p>
-                    <p>Permis B</p>
-                </div>
-            </div>
-        </div>
+        </section>
     </main>
 
     <footer>
