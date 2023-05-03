5b94d5752e97e10340ce64df26511830b4228f3f
Zanetti Jordan <jzanetti1@outlook.fr>
2023-05-03 04:57:14 +0200

    Add projects.json and base script JS to display projects automatically in the future

---
+++ scripts/projects.js
@@ -0,0 +1,7 @@
+const projects = await fetch ("projects.json").then(projects => projects.json());
+
+function generateProjetcs(projects){
+    for (let  i = 0; i < projects.lenght; i++){
+         
+    }
+}
